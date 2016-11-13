4607cad313af20e127d4326fd5097fd56c87702c
Prodge <prodge@prodge.net>
2016-11-13 15:37:40 +0000

    Updated to new graphics card

--- .screenlayout/og-main-left-portrait.sh
+++ .screenlayout/og-main-left-portrait.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-0 --off --output DVI-I-1 --off --output DVI-I-0 --off --output DVI-I-3 --mode 1680x1050 --pos 0x0 --rotate left --output DVI-I-2 --mode 1920x1080 --pos 1050x148 --rotate normal
+xrandr --output DVI-D-0 --mode 1680x1050 --pos 0x0 --rotate left --output HDMI-0 --off --output DVI-I-1 --mode 1920x1080 --pos 1050x184 --rotate normal --output DVI-I-0 --off --output DP-1 --off --output DP-0 --off
