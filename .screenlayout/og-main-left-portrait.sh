4c61b7aea3c771a67528faa72dbb7a6c278b6e53
Prodge <prodge@prodge.net>
2016-11-19 21:59:53 +0000

    Added refresh rate to 4k monitor

--- .screenlayout/og-main-left-portrait.sh
+++ .screenlayout/og-main-left-portrait.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --mode 1680x1050 --pos 0x0 --rotate left --output HDMI-0 --off --output DVI-I-1 --mode 1920x1080 --pos 1050x184 --rotate normal --output DVI-I-0 --off --output DP-1 --off --output DP-0 --off
+xrandr --output DP-3 --off --output DVI-I-0 --off --output HDMI-0 --mode 3840x2160 --rate 59.94 --pos 1080x48 --rotate normal --output DP-5 --off --output DP-4 --off --output DVI-I-1 --mode 1920x1080 --pos 0x0 --rotate left --output DP-2 --off --output DP-1 --off --output DP-0 --off
