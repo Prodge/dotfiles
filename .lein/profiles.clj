c57ac912eeb1544c76f8ccaca20d64f2ae50ffa9
Prodge <prodge@prodge.net>
2016-11-30 20:52:13 +0000

    Setup vim fireplace

---
+++ .lein/profiles.clj
@@ -0,0 +1,7 @@
+{:user {
+        :plugins  [[cider/cider-nrepl "0.14.0"]]
+        :dependencies [
+                       [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
+                       [cljfmt "0.5.1"]
+                       ]
+        }}
